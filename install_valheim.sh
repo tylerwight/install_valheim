63cb4896bb36a941bcf7105d148f9b81aa3db799
user <tylermwight@gmail.com>
2021-06-10 22:27:12 +0000

    fixed moving backup.sh

--- install_valheim.sh
+++ install_valheim.sh
@@ -15,10 +15,10 @@ sudo apt install lib32gcc1 steamcmd -y
 sudo ln -s /usr/games/steamcmd steamcmd
 sudo mkdir /mnt/valheim
 sudo mkdir /mnt/valheim/bak
+sudo cp ./backup.sh /mnt/valheim/backup.sh
 sudo chown -R valheim:valheim /mnt/valheim
 sudo su -c "steamcmd +login anonymous +force_install_dir /mnt/valheim +app_update 896660 validate +exit" -s /bin/sh valheim
 
-cp ./backup.sh /mnt/valheim/backup.sh
 sudo su -c "(crontab -l| grep -v -F '$cronjob'; echo '$cronjob') | crontab -" -s /bin/sh valheim
 
 
